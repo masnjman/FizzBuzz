eeac478467aaadaed6457e961ec6f10279d844d4
Mason Goslee <mjgqhq@umsystem.edu>
2023-03-08 15:38:42 -0600

    improved efficiency
    used 'not' instead of '==0' for condition checking

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,11 +1,11 @@
 for x in range(1, 101):
-    if(x%3 == 0 and x%5 == 0):
+    if(not x%3 and not x%5):
         print("FizzBuzz")
         continue
-    elif(x%3 == 0):
+    elif(not x%3):
         print("Fizz")
         continue
-    elif(x%5 == 0):
+    elif(not x%5):
         print("Buzz")
         continue
     else:
